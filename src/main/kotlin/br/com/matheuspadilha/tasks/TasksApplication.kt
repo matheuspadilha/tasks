dc53ed12d006c41c604585ae7f64efd7c7ffffeb
Matheus Padilha <mvpadilha94@gmail.com>
2024-02-11 14:58:30 -0300

    feat: Adding monitoring configuration

--- src/main/kotlin/br/com/matheuspadilha/tasks/TasksApplication.kt
+++ src/main/kotlin/br/com/matheuspadilha/tasks/TasksApplication.kt
@@ -2,10 +2,13 @@ package br.com.matheuspadilha.tasks
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import reactor.core.publisher.Hooks
 
 @SpringBootApplication
 class TasksApplication
 
 fun main(args: Array<String>) {
+	Hooks.enableAutomaticContextPropagation()
+
 	runApplication<TasksApplication>(*args)
 }
